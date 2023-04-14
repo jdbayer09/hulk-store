3f39a1b492274e51d32181402a89841d90a5bc14
Julian Bayer <jdbayer09@hotmail.com>
2023-04-14 16:36:34 -0500

    Pruebas unitarias para creacion de usuarios

--- src/test/java/com/softcaribbean/hulkstore/api/service/impl/UserServiceImplTest.java
+++ src/test/java/com/softcaribbean/hulkstore/api/service/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.softcaribbean.hulkstore.api.service.impl;
 
+import com.softcaribbean.hulkstore.api.exceptions.UserCreatedException;
 import com.softcaribbean.hulkstore.api.models.domain.User;
 import com.softcaribbean.hulkstore.api.models.entity.UserEntity;
 import com.softcaribbean.hulkstore.api.models.mapper.user.UserEntityDomainMapper;
@@ -11,10 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +31,8 @@ class UserServiceImplTest {
     private IUserRepository userRepository;
     @Mock
     private UserEntityDomainMapper userMapper;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @Test
     void when_findByEmail_is_not_null() {
@@ -46,11 +53,70 @@ class UserServiceImplTest {
         var email = "jdbayer@softcaribbean.com";
         var errorMessage = "No existe el usuario";
         when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> userService.findByEmail(email));
+
         try {
             var resp = userService.findByEmail(email);
             fail("Expected exception not thrown");
         } catch (Exception ex) {
             Assertions.assertTrue(ex instanceof UsernameNotFoundException);
+            Assertions.assertEquals(errorMessage, ex.getMessage());
+        }
+    }
+
+    @Test
+    void when_createUser() {
+        var user = new User();
+        user.setEmail("jdbayer@softcaribbean.com.com");
+        user.setName("Julian");
+        user.setPass("12345");
+        user.setLastName("Bayer");
+
+        var userEntity = new UserEntity();
+        userEntity.setEmail(user.getEmail());
+        userEntity.setName(user.getName());
+        userEntity.setActive(true);
+        userEntity.setLastName(user.getLastName());
+        userEntity.setPass("hashedPassword");
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+        when(passwordEncoder.encode(anyString())).thenReturn("hashedPassword");
+
+        var validUser = new User();
+        validUser.setEmail(user.getEmail());
+        validUser.setName(user.getName());
+        validUser.setActive(true);
+        validUser.setLastName(user.getLastName());
+        validUser.setPass("hashedPassword");
+
+        when(userMapper.userEntityToUser(any(UserEntity.class))).thenReturn(validUser);
+
+        var createdUser = userService.createUser(user);
+
+        Assertions.assertEquals(userEntity.getEmail(), createdUser.getEmail());
+        Assertions.assertEquals(userEntity.getName(), createdUser.getName());
+        Assertions.assertEquals(userEntity.getLastName(), createdUser.getLastName());
+        Assertions.assertEquals(userEntity.getPass(), createdUser.getPass());
+    }
+    @Test
+    void when_createUser_is_created() {
+        var user = new User();
+        user.setEmail("jdbayer@softcaribbean.com.com");
+        user.setName("Julian");
+        user.setPass("12345");
+        user.setLastName("Bayer");
+
+        var errorMessage = "El Usuario con el email: [" + user.getEmail() + "] ya se encuentra registrado";
+
+        when(passwordEncoder.encode(anyString())).thenReturn("hashedPassword");
+        when(userRepository.save(any(UserEntity.class))).thenThrow(new UserCreatedException(""));
+
+        Assertions.assertThrows(UserCreatedException.class, () -> userService.createUser(user));
+        try {
+            var createdUser = userService.createUser(user);
+            fail("Expected exception not thrown");
+        } catch (Exception ex) {
             Assertions.assertEquals(errorMessage, ex.getMessage());
         }
     }
