3c38126cca19e12b1f9c1a3ebbceb544ed26dcea
Julian Bayer <jdbayer09@hotmail.com>
2023-04-14 22:43:12 -0500

    Configuracion pruebas unitarias

--- src/test/java/com/softcaribbean/hulkstore/api/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/softcaribbean/hulkstore/api/service/impl/ProductServiceImplTest.java
@@ -1,24 +1,60 @@
 package com.softcaribbean.hulkstore.api.service.impl;
 
+import com.softcaribbean.hulkstore.api.models.domain.Product;
+import com.softcaribbean.hulkstore.api.models.entity.ProductEntity;
+import com.softcaribbean.hulkstore.api.models.mapper.product.ProductEntityDomainMapper;
+import com.softcaribbean.hulkstore.api.repository.IProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
+    @InjectMocks
+    private ProductServiceImpl productService;
+    @Mock
+    private IProductRepository productRepository;
+    @Mock
+    private ProductEntityDomainMapper productEntityDomainMapper;
 
     @Test
     void findAllProducts() {
-    }
+        List<ProductEntity> productEntities = new ArrayList<>();
+        var product1 = new ProductEntity();
+        product1.setId(1L);
+        product1.setName("Product 1");
+        productEntities.add(product1);
 
-    @Test
-    void createProduct() {
-    }
+        var product2 = new ProductEntity();
+        product1.setId(2L);
+        product1.setName("Product 2");
+        productEntities.add(product2);
 
-    @Test
-    void addProductStock() {
-    }
+        when(productRepository.findAll()).thenReturn(productEntities);
 
-    @Test
-    void removeProductStock() {
+        List<Product> expectedProducts = new ArrayList<>();
+        var expectedProduct1 = new Product();
+        expectedProduct1.setId(1L);
+        expectedProduct1.setName("Product 1");
+        expectedProducts.add(expectedProduct1);
+
+        var expectedProduct2 = new Product();
+        expectedProduct2.setId(2L);
+        expectedProduct2.setName("Product 2");
+        expectedProducts.add(expectedProduct2);
+
+        when(productEntityDomainMapper.productEntityToProduct(productEntities.get(0))).thenReturn(expectedProducts.get(0));
+        when(productEntityDomainMapper.productEntityToProduct(productEntities.get(1))).thenReturn(expectedProducts.get(1));
+
+        List<Product> actualProducts = productService.findAllProducts();
+        Assertions.assertEquals(expectedProducts, actualProducts);
     }
 }
